af9a17de1d9d2d6ee50233edc070754c3318a33d
Zaid Malik <mzaidmalik25@gmail.com>
2026-01-30 21:31:54 +0530

    learning genrics

---
+++ L5/index.ts
@@ -0,0 +1,26 @@
+// Genrics :- genric matlab abhi mere ko use karlo pr bad me type bta dena
+
+
+// type obj = {
+//     name: string;
+//     age: string
+// }
+
+
+// function abc<obj>(value: obj): obj {
+//     return value;
+// }
+
+// const res = abc<string>("zaid")
+// console.log(res)
+
+// interface ApiResponse <T>{
+//     data: T,
+//     status: number
+// }
+
+
+// const response:ApiResponse<string> = {
+//     data: "Hii",
+//     status: 200
+// }
