3cd16aea650184e1b0fc658a1185307744ab076d
Zaid Malik <mzaidmalik25@gmail.com>
2026-01-30 15:11:40 +0530

    L2 complete and understand enum tuple object array in ts

---
+++ L2/index.js
@@ -0,0 +1,15 @@
+// Object Array Tuple Enum
+// object
+// const person: {
+//     firstName: string;
+//     lastName: string
+// } = {
+//     firstName: "Zaid",
+//     lastName: "Malik"
+// }
+// console.log(person)
+// tuple :- fixed length + fixed types
+// const product: [string, number] = ["macbook", 90000];
+// console.log(product);
+var person = ["zaid", "malik", "mohdzaidmalik"];
+console.log(person);
---
+++ L2/index.ts
@@ -0,0 +1,33 @@
+// Object Array Tuple Enum
+
+// object
+
+// const person: {
+//     firstName: string;
+//     lastName: string
+// } = {
+//     firstName: "Zaid",
+//     lastName: "Malik"
+// }
+
+// console.log(person)
+
+// tuple :- fixed length + fixed types
+
+// const product: [string, number] = ["macbook", 90000];
+
+// console.log(product);
+
+// const person: string[] = ["zaid", "malik", "mohdzaidmalik"]
+// console.log(person)
+
+// enum :- Enum use hota hai jab hume pehle se pata ho ki
+// values limited & fixed hongi
+// aur unke alawa kuch bhi allow nahi karna.
+
+// enum Role {"User", "Admin"}
+
+// const person = {
+//     name: "Mohd Zaid Malik",
+//     role: Role.User
+// }
